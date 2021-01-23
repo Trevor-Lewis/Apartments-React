505eca00b630f72c82d6fef655db68c7606cf771
Trevor-Lewis <tenzingtitan@gmail.com>
2021-01-23 14:55:54 -0600

    App fully built. Strictly Front-End for now. Will Add backend soon.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Styles
+import './styles/footer.css'
+
+export default function Footer() {
+      return (
+            <div className="footer-wrapper">
+                  <div className="container">
+                        <div className="footer">
+                              <h2>Copyright Apartments Inc</h2>
+                        </div>
+                  </div>
+            </div>
+      )
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Styles
+import './styles/nav.css';
+
+export default function Navigation() {
+      return (
+            <div className="nav-container">
+                  <div className="container">
+                        <div className="nav-wrapper">
+                              <Link className="logo" to="/">
+                                    <h1>Apartments Inc</h1>
+                              </Link>
+
+                              <div className="nav-links">
+                                    <Link className="link" to="/gallery">
+                                          <h2>Gallery</h2>
+                                    </Link>
+
+                                    <Link className="link" to="/floorplans">
+                                          <h2>Floorplans</h2>
+                                    </Link>
+
+                                    <Link className="link" to="/apply">
+                                          <h2>Apply</h2>
+                                    </Link>
+
+                                    <Link className="link" to="/contact">
+                                          <h2>Contact</h2>
+                                    </Link>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      )
+}
